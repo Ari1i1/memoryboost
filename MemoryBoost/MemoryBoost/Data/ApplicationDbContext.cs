fd129a8144cf1fff2e871ed8bb519f10d71159fe
Ari1i1 <afanask1nanyc@gmail.com>
2021-04-27 00:26:22 +0700

    Added login's view, fixed login and logout's paths

--- MemoryBoost/MemoryBoost/Data/ApplicationDbContext.cs
+++ MemoryBoost/MemoryBoost/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using MemoryBoost.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,7 @@ using System.Text;
 
 namespace MemoryBoost.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
